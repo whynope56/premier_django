948fc55838bdf073adc895cd0c254f7c4b33709c
alexis <charron.alexis@gmail.com>
2020-04-20 02:11:59 +0200

    Changement de fichier html pour Home Page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-        path('', views.post_list, name='post_list'),
+urlpatterns = [        
+        path('', views.home_page, name='home_page'),
+        path('postlist', views.post_list, name='post_list'),
     ]
