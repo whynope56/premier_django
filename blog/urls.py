0cefdce5667afa61c39866b10b6da4bf322d2413
alexis <charron.alexis@gmail.com>
2020-04-25 21:03:41 +0200

    Ajout des pages Anniv

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+# anniv_patterns = ([
+#     path('', views.view_choix, name='view_choix'),
+#     path('bonchoix/', views.bonchoix, name='bonchoix'),
+#     path('naaaaaaaan/', views.naaaaaaaan, name='naaaaaaaan'),
+# ], 'unknown')
 
 urlpatterns = [        
         path('', views.home_page, name='home_page'),
-        path('postlist', views.post_list, name='post_list'),
+        path('postlist/', views.post_list, name='post_list'),
+        path('post/<int:pk>/', views.post_detail, name='post_detail'),
+        path('unknown/', views.choix, name='choix'),
+        path('unknown/bonchoix/', views.bonchoix, name='bonchoix'),
+        path('unknown/naaaaaaaan/', views.naaaaaaaan, name='naaaaaaaan'),
     ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,16 @@ def post_list(request):
 
 def home_page(request):
     return render(request, 'blog/home_page.html')
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def choix(request):
+    return render(request, 'blog/choix.html')
+
+def naaaaaaaan(request):
+    return render(request, 'blog/naaaaaaaan.html')
+
+def bonchoix(request):
+    return render(request, 'blog/bonchoix.html')
