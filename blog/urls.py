1e3351098080b1c118f0d3e3d06c5d67ec42a2bd
alexis <charron.alexis@gmail.com>
2020-05-09 02:13:51 +0200

    Ajout de nouveaux HTML

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+#from blog.views import YogaView
 
 # anniv_patterns = ([
 #     path('', views.view_choix, name='view_choix'),
@@ -11,9 +12,18 @@ urlpatterns = [
         path('', views.home_page, name='home_page'),
         path('postlist/', views.post_list, name='post_list'),
         path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
         path('unknown/', views.choix, name='choix'),
         path('unknown/bonchoix/', views.bonchoix, name='bonchoix'),
         path('unknown/naaaaaaaan/', views.naaaaaaaan, name='naaaaaaaan'),
         path('unknown/dategame/', views.dategame, name='dategame'),
+
+        path('yoga/vinyasa/', views.yoga_vinyasa, name='yoga_vinyasa'),
+        path('yoga/yin/', views.yoga_yin, name='yoga_yin'),
+        path('yoga/pre-post-natal/', views.yoga_natal, name='yoga_natal'),
+        path('yoga/aerien/', views.yoga_aerien, name='yoga_aerien'),
+        path('yoga/planning/', views.yoga_planning, name='yoga_planning'),
+        path('yoga/entreprise/', views.yoga_entreprise, name='yoga_entreprise'),
+        path('yoga/intentions/', views.yoga_intentions, name='yoga_intentions'),
     ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.views import View
 from .models import Post
 
 
@@ -27,3 +28,29 @@ def bonchoix(request):
 
 def dategame(request):
     return render(request, 'blog/dategame.html')
+
+def yoga_vinyasa(request):
+    return render(request, 'blog/yoga_vinyasa.html')
+
+def yoga_yin(request):
+    return render(request, 'blog/yoga_yin.html')
+
+def yoga_natal(request):
+    return render(request, 'blog/yoga_natal.html')
+
+def yoga_aerien(request):
+    return render(request, 'blog/yoga_aerien.html')
+
+def yoga_planning(request):
+    return render(request, 'blog/yoga_planning.html')
+
+def yoga_entreprise(request):
+    return render(request, 'blog/yoga_entreprise.html')
+
+def yoga_intentions(request):
+    return render(request, 'blog/yoga_intentions.html')
+
+# class YogaView(View):
+
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'blog/vinyasa.html')
